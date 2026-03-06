b2675f034c5de09ee1196bb0bea359a13ed492b6
Edu-0 <eduardojose.de.souza332@gmail.com>
2026-03-06 16:27:04 -0300

    feat: finished MixColumns function from AES

--- aes-learning/main.py
+++ aes-learning/main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import s_box as sb
 import shift_rows as sr
+import mix_columns as mc
+
 
 def print_hex(bb):
     for i in range(bb.shape[0]):
@@ -17,18 +19,25 @@ def s_box_block(bb):
 
     return res_array
 
+
 def shift_rows(bb):
     return sr.shift_rows(s_box_block(bb))
+
+
+def mix_columns(bb):
+    return mc.mix_columns(sr.shift_rows(s_box_block(bb)))
+
+
 def get_test_block():
     return state
 
 
-byte_block = [
+byte_block = np.array([
     [0xEA, 0x04, 0x65, 0x85],
     [0x83, 0x45, 0x5D, 0x96],
     [0x5C, 0x33, 0x98, 0xB0],
-    [0xF0, 0x2D, 0xAD, 0xC5]]
+    [0xF0, 0x2D, 0xAD, 0xC5]],
+    dtype=int)
 
-byte_block = np.array(byte_block)
-state = shift_rows(byte_block)
+state = mix_columns(byte_block)
 print_hex(state)
