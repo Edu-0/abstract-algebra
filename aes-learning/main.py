a56164b17a42335ee417c4cefd3a0c0aa9dccb0c
Edu-0 <eduardojose.de.souza332@gmail.com>
2026-03-06 16:52:44 -0300

    feat: finished the algorithm to cipher block

--- aes-learning/main.py
+++ aes-learning/main.py
@@ -2,6 +2,8 @@ import numpy as np
 import s_box as sb
 import shift_rows as sr
 import mix_columns as mc
+import add_round_key as ark
+import abstract_algebra as aa
 
 
 def print_hex(bb):
@@ -28,16 +30,40 @@ def mix_columns(bb):
     return mc.mix_columns(sr.shift_rows(s_box_block(bb)))
 
 
+def add_round_key(bb, round_key):
+    return ark.add_round_key(mc.mix_columns(sr.shift_rows(s_box_block(bb))), round_key)
+
+def cryptograph_block(bb, round_key):
+    return add_round_key(bb, round_key)
+
 def get_test_block():
-    return state
+    return ciphered_byte
 
 
-byte_block = np.array([
-    [0xEA, 0x04, 0x65, 0x85],
-    [0x83, 0x45, 0x5D, 0x96],
-    [0x5C, 0x33, 0x98, 0xB0],
-    [0xF0, 0x2D, 0xAD, 0xC5]],
-    dtype=int)
+def get_test_key():
+    return test_key
 
-state = mix_columns(byte_block)
-print_hex(state)
+
+byte_block = np.array(
+    [
+        [0xEA, 0x04, 0x65, 0x85],
+        [0x83, 0x45, 0x5D, 0x96],
+        [0x5C, 0x33, 0x98, 0xB0],
+        [0xF0, 0x2D, 0xAD, 0xC5]
+    ],
+    dtype=int
+)
+
+test_key = np.array(
+    [
+        [0xAC, 0x19, 0x28, 0x57],
+        [0x77, 0xFA, 0xD1, 0x5C],
+        [0x66, 0xDC, 0x29, 0x00],
+        [0xF3, 0x21, 0x41, 0x6A]
+    ],
+    dtype=int
+)
+
+ciphered_byte = cryptograph_block(byte_block, test_key)
+
+print_hex(ciphered_byte)
