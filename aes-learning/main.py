e9df87fcb6e4ef304a7dd6610ba4f23a93b49a0e
Edu-0 <eduardojose.de.souza332@gmail.com>
2026-03-06 14:41:40 -0300

    feat: now added byte block testing

---
+++ aes-learning/main.py
@@ -0,0 +1,29 @@
+import numpy as np
+import s_box as sb
+
+
+def print_hex(bb):
+    for i in range(bb.shape[0]):
+        for j in range(bb.shape[1]):
+            print(f"{bb[i, j]:02X}", end=" ")
+        print()
+
+
+def s_box_block(bb):
+    res_array = np.zeros((4, 4), dtype=int)
+    for i in range(res_array.shape[0]):
+        for j in range(res_array.shape[1]):
+            res_array[i][j] = sb.aes_sbox(bb[i][j])
+
+    return res_array
+
+
+byte_block = [
+    [0xEA, 0x04, 0x65, 0x85],
+    [0x83, 0x45, 0x5D, 0x96],
+    [0x5C, 0x33, 0x98, 0xB0],
+    [0xF0, 0x2D, 0xAD, 0xC5]]
+
+byte_block = np.array(byte_block)
+
+print_hex(s_box_block(byte_block))
