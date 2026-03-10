67049ee771da2189edb83a3badc9574c01735305
Edu-0 <eduardojose.de.souza332@gmail.com>
2026-03-10 14:41:46 -0300

    feat: added string to bin converter and list of binary normalizer

---
+++ aes-learning/text_converter.py
@@ -0,0 +1,25 @@
+import numpy as np
+from sympy import ceiling
+
+text1 = "João comeu feijão e não fez bem! ≤, ∞, ±, √"
+text2 = "An even longer test text with more than 16 bits, 51"
+text = "Olá Mundo"
+
+
+# Simple conversion to binary
+def string_to_bin(text):
+    return list(text.encode("UTF-8"))
+
+
+# This function makes sure that the list has a size multiple of 16 for it to be able to divide into blocks of 4x4
+def normalize_list(bin_list):
+    len_bin_list = len(bin_list)
+    missing = 16 - len_bin_list % 16
+    if missing == 0:
+        missing = 16
+    for i in range(missing):
+        bin_list.append(missing)
+    return bin_list
+
+
+bin_list = normalize_list(string_to_bin(text))
